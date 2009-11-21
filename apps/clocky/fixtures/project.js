fa3a3f0c8decc3c80e2a02a46897a4606575add7
Bruz Marzolf <bruz@edgenic.com>
2009-11-20 16:39:54 -0800

    Update fixtures so that they work

--- apps/clocky/fixtures/project.js
+++ apps/clocky/fixtures/project.js
@@ -11,36 +11,19 @@ Clocky.Project.FIXTURES = [
   { guid: "project-1",
     name: "Trivial Timer with RestfulX",
     projectStatus: "mostly done",
-    startDate: "2009-09-01"},
+    startDate: "2009-09-01",
+    projectSessions: []},
 
   { guid: "project-2",
     name: "Trivial Timer with Rails",
     projectStatus: "done",
-    startDate: "2009-08-25"},
+    startDate: "2009-08-25",
+    projectSessions: []},
 
   { guid: "project-3",
     name: "Trivial Timer with SproutCore",
     projectStatus: "started",
-    startDate: "2009-09-11"}
-
-  // { guid: 1,
-  //   firstName: "Michael",
-  //   lastName: "Scott" },
-  //
-  // { guid: 2,
-  //   firstName: "Dwight",
-  //   lastName: "Schrute" },
-  //
-  // { guid: 3,
-  //   firstName: "Jim",
-  //   lastName: "Halpert" },
-  //
-  // { guid: 4,
-  //   firstName: "Pam",
-  //   lastName: "Beesly" },
-  //
-  // { guid: 5,
-  //   firstName: "Ryan",
-  //   lastName: "Howard" }
+    startDate: "2009-09-11",
+    projectSessions: ["projectSession-1","projectSession-2"]}
 
 ];
--- apps/clocky/fixtures/project_session.js
+++ apps/clocky/fixtures/project_session.js
@@ -10,34 +10,14 @@ Clocky.ProjectSession.FIXTURES = [
 
   { guid: "projectSession-1",
     description: "Write some models and fixtures",
-    startTime: "2009-09-11 17:30", //  2009-09-11T17:19:49-07:00",
-    endTime: "2009-09-11 19:00",
+    startTime: "2009-09-11T17:30:00",
+    endTime: "2009-09-11T19:00:00",
     project: "project-3"},
 
   { guid: "projectSession-2",
     description: "Finish this masterful app",
-    startTime: "2009-09-14 06:30",
-    endTime: "2009-09-14 07:30",
+    startTime: "2009-09-14T06:30:00",
+    endTime: "2009-09-14T07:30:00",
     project: "project-3"}
-
-  // { guid: 1,
-  //   firstName: "Michael",
-  //   lastName: "Scott" },
-  //
-  // { guid: 2,
-  //   firstName: "Dwight",
-  //   lastName: "Schrute" },
-  //
-  // { guid: 3,
-  //   firstName: "Jim",
-  //   lastName: "Halpert" },
-  //
-  // { guid: 4,
-  //   firstName: "Pam",
-  //   lastName: "Beesly" },
-  //
-  // { guid: 5,
-  //   firstName: "Ryan",
-  //   lastName: "Howard" }
 
 ];
